e045d3b94dd4e4f1fc25d63fb4b0c2ae274ff170
Basant <basant.kumar@gslab.com>
2019-02-10 11:12:50 +0530

    add models to store data related to 2nd and 3rd API. Modify the mail template

--- notification/notify/models.py
+++ notification/notify/models.py
@@ -1,4 +1,23 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
+class EmailsInfo(models.Model):
+    subject = models.CharField(max_length=1000)
+    user_mail_id = models.EmailField()
+    from_mail_id = models.EmailField(default=settings.EMAIL_HOST_USER)
+    sent_datetime = models.DateTimeField(auto_now=True)
+    user_name = models.CharField(max_length=255)
+
+
+class EmailActivity(models.Model):
+    email_opened = models.BooleanField(default=False)
+    email_clicked = models.BooleanField(default=False)
+    email_delivered = models.BooleanField(default=False)
+    email_unsubscribed = models.BooleanField(default=False)
+
+
+class CustomerView(models.Model):
+    campaign_id = models.AutoField(primary_key=True)
+    customer_email = models.OneToOneField(EmailsInfo, on_delete=models.SET_NULL, null=True)
+    customer_activity = models.OneToOneField(EmailActivity, on_delete=models.SET_NULL, null=True)
--- notification/notify/views.py
+++ notification/notify/views.py
@@ -9,7 +9,7 @@ from rest_framework.parsers import JSONParser
 from notify.utils import validate_user_email
 from notify.helpers import send_mail
 from rest_framework.response import Response
-
+from notify.models import CustomerView, EmailActivity, EmailsInfo
 
 @api_view(['POST'])
 def send_user_email(request):
@@ -33,10 +33,24 @@ def send_user_email(request):
 
     for user_mail in user_email_content:
 
+        email_activity = EmailActivity()
+        email_activity.save()
+
+        email_info = EmailsInfo(subject = subject, user_mail_id = user_mail['email_id'], user_name = user_mail['username'] )
+        email_info.save()
+
+        customer_view = CustomerView(customer_email = email_info, customer_activity = email_activity)
+        customer_view.save()
+
+
+        click_link = 'http://127.0.0.1:8000/notify/activity/click/'+ str(customer_view.campaign_id) + '/' + str(user_mail['email_id'])
+        unsubscribe_link = 'http://127.0.0.1:8000/notify/unsubscribe/'+ str(customer_view.campaign_id) + '/' + str(user_mail['email_id'])
+        open_link = 'http://127.0.0.1:8000/notify/activity/open/'+ str(customer_view.campaign_id) + '/' + str(user_mail['email_id'])
         parameter_dict = {
             'html_template_content_path' : html_template_content_path,
             'sender_mail' : sender_mail, 'subject' : subject,
-            'mail_id' : [user_mail['email_id']], 'content' : user_mail['content']
+            'mail_id' : [user_mail['email_id']], 'content' : user_mail['content'],
+            'click_link' : click_link, 'open_link' : open_link, 'unsubscribe_link' : unsubscribe_link
         }
 
         send_mail(parameter_dict)
